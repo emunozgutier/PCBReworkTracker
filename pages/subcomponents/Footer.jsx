3a57d50da281057e3ff516a41c27f7b869e42d52
emunozgutier <emunozgutier@gmail.com>
2026-03-25 13:54:31 -0700

    Restarting project: Clear all content

--- pages/subcomponents/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer">
-            <p>&copy; 2026 PCB Rework Tracker. Powered by Flask & React.</p>
-        </footer>
-    );
-}
-
-window.Footer = Footer;
